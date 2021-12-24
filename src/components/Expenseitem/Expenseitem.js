4c818adb7839db7023ad2210e0a4d6f5fa3dabd3
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2021-12-24 14:19:21 -0500

    updated value by clicking each button

--- src/components/Expenseitem/Expenseitem.js
+++ src/components/Expenseitem/Expenseitem.js
@@ -5,6 +5,25 @@ import '../Expenseitem/Expenseitem.scss';
 
 function ExpenseItem(props) {
   const expenseData = [...props.data];
+  const [title, SetTitle] = useState({
+    activeObject: null,
+    objects: [...props.data],
+  });
+  const [clicked, SetClicked] = useState({});
+
+  const btnHandler = (index) => {
+    SetTitle({ ...title, activeObject: title.objects[index] });
+    console.log(title);
+    // {title.objects[i] === title.activeObject ? 'updated' : props.data.title}
+  }
+
+  const setTitle = (index) => {
+    SetClicked(item => ({
+      ...item,
+      [index]: !item[index]
+    }));
+    console.log(clicked);
+  }
 
   return (
     <>
@@ -18,9 +37,10 @@ function ExpenseItem(props) {
             <Card className="expense-item" key={i}>
               <ExpenseDate month={month} year={year} day={day} />
               <div className="expense-item__description">
-                <h2>{item.title}</h2>
+                <h2>{clicked[i] ? 'updated' : item.title}</h2>
                 <p className="expense-item__price">${item.amount}</p>
               </div>
+              <button onClick={() => { setTitle(i) }}>Change title</button>
             </Card>
           )
         })
